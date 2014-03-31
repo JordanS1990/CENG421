dae500cb89e7751f4da355650cda6ec8e1700969
Derek Binnersley <dbinners@c77.seng.uvic.ca>
2014-03-31 12:00:25 -0700

    Changed a couple things in process

--- process.m
+++ process.m
@@ -12,45 +12,49 @@ function [ combined, newblue ] = process( input_name )
     blue = imin(:,:,3);
     blue(mask < 255) = 0;
     
-    %hist = imhist(blue)
-    %hist(1) = 0;
-    %figure('name', 'bluehist')
-    %plot(hist)
+    hist = imhist(blue)
+    hist(1) = 0;
+    figure('name', 'bluehist')
+    plot(hist)
     
-    %hist = imhist(green)
-    %hist(1) = 0;
-    %figure('name', 'greenhist')
-    %plot(hist)
+    hist = imhist(green)
+    hist(1) = 0;
+    figure('name', 'greenhist')
+    plot(hist)
     
-    %hist = imhist(red)
-    %hist(1) = 0;
-    %figure('name', 'redhist')
-    %plot(hist)
+    hist = imhist(red)
+    hist(1) = 0;
+    figure('name', 'redhist')
+    plot(hist)
 
-    %figure('name','red')
+    figure('name','red')
     newred = red > 150;
-    %imshow(newred)
+    imshow(newred)
     
-    %figure('name','green')
+    figure('name','green')
     newgreen = green > 150;
-    %imshow(newgreen)
+    imshow(newgreen)
 
-    %figure('name','blue')
+    figure('name','blue')
     newblue = blue > 140;
-    %imshow(newblue)
+    imshow(newblue)
     
     figure('name','combined')
     combined = newgreen & newblue;
     imshow(combined)
     
+    figure('name', 'maskremoved')
+    maskremoved = combined;
+    maskremoved(mask==0) = 1;
+    imshow(maskremoved);
     
     manual_name = strrep(input_name, '.000Z.jpg', '-watermap.jpg');
     manual_image = imread(manual_name);
     accuracy(combined,manual_image,mask)
     
-    %figure('name','adaptive')
-    %a = adaptivethreshold(green, 7,0.03,0);
-    %imshow(a)
+    figure('name','adaptive')
+    a = adaptivethreshold(green, 7,0.03,0);
+    imshow(a)
     
     %figure('name','red')
     %ot = uint8((otsu(red,10)-1)*(28.3));
